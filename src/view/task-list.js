47a36b92d23b88e98a6a535af6127a4001a3d3ce
Serenya Dyadev <3684182@gmail.com>
2020-08-13 15:54:42 +0300

    часть 1ая

--- src/view/task-list.js
+++ src/view/task-list.js
@@ -1,7 +1,7 @@
 import {createElement} from "../utils.js";
 
 const createTaskListTemplate = () => {
-  return `<div class="board__tasks"></div>`
+  return `<div class="board__tasks"></div>`;
 };
 
 export default class TaskList {
